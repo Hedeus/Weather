f69210dccded3c31be9260effb7e0e8552c5f224
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-05-14 14:57:01 +0300

    Инфраструктура для создания хоста приложения

--- Weather/App.xaml.cs
+++ Weather/App.xaml.cs
@@ -1,9 +1,21 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using Weather.ViewModels;
 
 namespace Weather
 {
-    
+
     public partial class App : Application
     {
+        
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            //services.AddSingleton<DataService>();
+            //services.AddSingleton<CountriesStatisticViewModel>();
+            services.AddSingleton<WelcomeWindowModel>();
+        }
+
     }
 }
--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Weather
 {
@@ -14,6 +12,22 @@ namespace Weather
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(Args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
         }
     }
 }
